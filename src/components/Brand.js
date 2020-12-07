e024e787312f083198f2fa645cdf0313e25549d0
Marcio <marciofcb@gmail.com>
2020-12-07 18:56:31 -0300

    Change: Brand.js & colors

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const styles = {
-	float: 'left',
-	width: '15vw'
+	color: '#fff',
+	fontFamily: 'Raleway',
+	fontSize: 'min(2.7vw, 2em)'
 }
 
 function Brand() {
 	return <>
 		<Link to="/" style={styles} >
-			<img style={{ width: '100%' }} src='/images/ecommerce.png' alt='logo' />
+			<p>CommerceHouse</p>
 		</Link>
 	</>
 }
