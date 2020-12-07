25c5f1e7c7b99403b6aeae7b9f988f75e02fee88
Marcio <marciofcb@gmail.com>
2020-12-07 19:02:39 -0300

    Add: EmptyCartView.js

--- src/components/CartView.js
+++ src/components/CartView.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { CartContext } from '../context/cartContext';
 import { Link } from 'react-router-dom';
+import EmptyCartView from './EmptyCartView';
 import './CartView.css';
 
 function CartView() {
@@ -16,18 +17,6 @@ function CartView() {
 			setMount(!mount)
 		}, 1000)
 	}, [])
-
-	function EmptyCartView() {
-		return (
-		<section style={{ margin: '2%' }}>
-			<img src='/images/emptyCart.png' style={{ display: 'block', margin: 'auto' }} alt="" />
-			<h1 style={{ textAlign: 'center', fontSize: '3em' }}>Empty Cart</h1>
-			<div style={{ textAlign: 'center', fontSize: '1.5em', paddingTop: '10vh' }}>
-				<Link to="/">← Ir a Página Principal</Link>
-			</div>
-		</section>
-		)
-	}
 
 	function ItemCartList() {
 		return (
